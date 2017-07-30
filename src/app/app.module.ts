68c05abad67d019f36d87041f677170f2432487b
Pablo Jose Bergonzi <pablo.bergonzi@globant.com>
2017-07-30 16:23:01 -0300

    added i18n support & createg language service in order to detect the browser active language and provide a default fallback language

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import {HttpClientModule, HttpClient} from '@angular/common/http';
+import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 
 import { routes } from './app.router';
 import { AppComponent } from './app.component';
@@ -10,6 +13,11 @@ import { QuotientComponent } from './components/quotient/quotient.component';
 import { StoreModule } from '@ngrx/store';
 import { cardReducer } from 'app/reducers/card.reducer';
 import { ProductSelectionComponent } from './components/wizzard/product-selection/product-selection.component';
+import { LanguageService } from 'app/services/language/language.service';
+
+export function HttpLoaderFactory(http: HttpClient) {
+    return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
@@ -25,9 +33,17 @@ import { ProductSelectionComponent } from './components/wizzard/product-selectio
     FormsModule,
     StoreModule.provideStore({ 
       card: cardReducer 
-    })
+    }),
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient]
+      }
+    })    
   ],
-  providers: [],
+  providers: [LanguageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
