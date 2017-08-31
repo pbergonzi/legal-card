6515a3a4be0300ac74d255f6fb6d964172f07341
meanMonk <sahilkashetwar24@gmail.com>
2017-08-31 20:02:42 +0530

    Code changes for the mobile + tablet responsive website

--- src/app/insurance/insurance.component.ts
+++ src/app/insurance/insurance.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var $: any;
+
 @Component({
   selector: 'app-insurance',
   templateUrl: './insurance.component.html',
@@ -10,6 +12,16 @@ export class InsuranceComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    // $(".mobile-navigation").append($(".main-navigation .menu").clone());
+
+    // Mobile menu toggle
+    $(".menu-toggle").click(function(){
+      $(".mobile-navigation").slideToggle();
+    });
+
+    $(".main-navigation .menu li>a").click(function(){
+      $(".mobile-navigation").slideToggle();
+    });
   }
 
 }
