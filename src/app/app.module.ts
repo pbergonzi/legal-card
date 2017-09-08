19665228a3f6d3b4d7dd911e97c975dd89347e5f
Pablo Jose Bergonzi <pablo.bergonzi@globant.com>
2017-09-08 12:22:19 -0300

    finished migration to new insutance template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 //import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
@@ -9,20 +10,40 @@ import { environment } from '../environments/environment';
 
 import { routes } from 'app/app.router';
 import { AppComponent } from 'app/app.component';
-import { QuotientComponent } from 'app/components/quotient/quotient.component';
-import { ProductSelectionComponent } from 'app/components/wizzard/product-selection/product-selection.component';
-import { PersonalDataComponent } from 'app/components/wizzard/personal-data/personal-data.component';
 
 import { StoreModule } from '@ngrx/store';
 import { cardReducer } from 'app/reducers/card.reducer';
 import { CardService } from 'app/services/card/card.service';
 import { PaypalComponent } from './components/paypal/paypal.component';
 import { MyDatePickerModule } from 'mydatepicker';
-import { YoutubePlayerMiniModule }  from 'ng2-youtube-player-mini';
+import { InsuranceHeaderComponent } from 'app/components/insurance-header/insurance-header.component';
+import { InsuranceFooterComponent } from 'app/components/insurance-footer/insurance-footer.component';
+import { InsuranceHomeComponent } from 'app/components/insurance-home/insurance-home.component';
+import { InsuranceContactComponent } from 'app/components/insurance-contact/insurance-contact.component';
+import { InsuranceAboutusComponent } from 'app/components/insurance-aboutus/insurance-aboutus.component';
+import { InsuranceResourceComponent } from 'app/components/insurance-resource/insurance-resource.component';
+import { InsurancePlansComponent } from 'app/components/insurance-plans/insurance-plans.component';
+import { MapComponent } from 'app/components/map/map.component';
+import { PlanFilterPipe } from 'app/services/pipes/planFilter.pipe';
+import { QuotientComponent } from 'app/components/quotient/quotient.component';
+import { ProductSelectionComponent } from 'app/components/wizzard/product-selection/product-selection.component';
+import { PersonalDataComponent } from 'app/components/wizzard/personal-data/personal-data.component';
+import { CarouselModule } from 'ngx-bootstrap';
+import { AgmCoreModule } from '@agm/core';
 
 @NgModule({
   declarations: [
     AppComponent,
+    InsuranceHeaderComponent,
+    InsuranceFooterComponent,
+    InsuranceHomeComponent,
+    InsuranceContactComponent,
+    InsuranceAboutusComponent,
+    InsuranceResourceComponent,
+    InsurancePlansComponent,
+    MapComponent,
+    PlanFilterPipe,
+    QuotientComponent,
     ProductSelectionComponent,
     PersonalDataComponent,
     PaypalComponent
@@ -34,11 +55,16 @@ import { YoutubePlayerMiniModule }  from 'ng2-youtube-player-mini';
     StoreModule.provideStore({
       card: cardReducer
     }),
-    YoutubePlayerMiniModule,
     MyDatePickerModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    CommonModule,
+    ReactiveFormsModule,
+    CarouselModule.forRoot(),
+    AgmCoreModule.forRoot({
+      apiKey : 'AIzaSyCbRPhVlxgVwBC0bBOgyB-Dn_K8ONrxb_g'  //the api key use here is dummy please replace this with your own api key
+    }),
   ],
   providers: [
     CardService
--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -5,17 +5,35 @@ import { AppComponent } from './app.component';
 import { ProductSelectionComponent } from 'app/components/wizzard/product-selection/product-selection.component';
 import { PersonalDataComponent } from 'app/components/wizzard/personal-data/personal-data.component';
 
-export const router: Routes = [
-  // { path: '', component: HomeComponent, pathMatch: 'full'},
+import { InsuranceHomeComponent } from 'app/components/insurance-home/insurance-home.component';
+import { InsuranceContactComponent } from 'app/components/insurance-contact/insurance-contact.component';
+import { InsuranceAboutusComponent } from 'app/components/insurance-aboutus/insurance-aboutus.component';
+import { InsuranceResourceComponent } from 'app/components/insurance-resource/insurance-resource.component';
+import { InsurancePlansComponent } from 'app/components/insurance-plans/insurance-plans.component';
 
-  { path: 'product-selection', component: ProductSelectionComponent },
 
-  { path: 'personal-data', component: PersonalDataComponent },
-
-  { path: '', loadChildren: 'app/components/insurance.module#InsuranceModule' },
-
-  { path: '**', redirectTo: '', pathMatch: 'full'}
-
+const router: Routes = [
+  {
+      path: '', component : InsuranceHomeComponent
+  },
+  {
+    path: 'contact', component : InsuranceContactComponent
+  },
+  {
+    path: 'about', component : InsuranceAboutusComponent
+  },
+  {
+    path: 'resources', component : InsuranceResourceComponent
+  },
+  {
+    path: 'plans', component : InsurancePlansComponent
+  },
+  { 
+    path: 'product-selection', component: ProductSelectionComponent 
+  },
+  { 
+    path: 'personal-data', component: PersonalDataComponent 
+  }
 ];
 
 export const routes: ModuleWithProviders = RouterModule.forRoot(router);
