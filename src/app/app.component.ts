3a6b57f2bb50c2e2bf34e3598355fc49b09d8e7c
Pablo <pablo@Pablos-MacBook-Air.local>
2017-10-13 23:58:59 -0300

    Add mobile menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,17 +24,6 @@ export class AppComponent implements OnInit {
   }
   
   ngOnInit() {
-    console.log(appName +  ' : ' + appVersion);
     
-    // $(".mobile-navigation").append($(".main-navigation .menu").clone());
-
-    // Mobile menu toggle
-    $(".menu-toggle").click(function(){
-      $(".mobile-navigation").slideToggle();
-    });
-
-    $(".main-navigation .menu li>a").click(function(){
-      $(".mobile-navigation").slideToggle();
-    });
   }
 }
